76d5c99b378d98fc2bb9f49cb502a128a2796338
FiluxLighting <juan@fitenergy.es>
2026-08-10 10:22:12 +0200

    style: mostrar 'Rachas X' en lugar de flecha hacia arriba y retirar etiqueta de estado en la vista por horas

--- main.js
+++ main.js
@@ -488,9 +488,8 @@ function initSpotForecasts() {
           <span class="hc-time">${hourLabel}</span>
           <span class="hc-arrow" title="${degreesToCompass(d)}">${degreesToArrow(d)}</span>
           <span class="hc-wind">${Math.round(w)} kts</span>
-          <span class="hc-gust">↑${Math.round(g)}</span>
+          <span class="hc-gust">Rachas ${Math.round(g)}</span>
           <span class="hc-temp">${Math.round(t)}°</span>
-          <span class="hc-status">${st.label}</span>
         </div>`;
     }
 
@@ -562,7 +561,7 @@ function initSpotForecasts() {
               <span class="fc-day">${formatDay(dateStr, i)}</span>
               <span class="fc-arrow" title="${degreesToCompass(d)}">${degreesToArrow(d)}</span>
               <span class="fc-wind">${Math.round(w)} kts</span>
-              <span class="fc-gust">↑${Math.round(g)}</span>
+              <span class="fc-gust">Rachas ${Math.round(g)}</span>
               <span class="fc-temp">${Math.round(tH)}° / ${Math.round(tL)}°</span>
               <span class="fc-status">${st.label}</span>
             </div>`;
